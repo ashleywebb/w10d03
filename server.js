03795ddfbbf59fcb441242c84a75472244d1800e
ashleywebb <ashleywebb112@gmail.com>
2022-02-15 11:45:16 -0700

    minor changes!

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ app.get('/greeting/:name', (req, res) => {
     res.send(`<h1>Hi ${name}!<h1>`)
 })
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    let tip = req.params.total
+    let tipPercentage = (req.params.tipPercentage)/100
+    let tipEnd = tip * tipPercentage
+    res.send(`<h1>Your tip is $${tipEnd}.</h1>`)
+})
+
+app.get('/magic/:question', (req, res) => {
+    
+})
+
 // listening on port 3000
 app.listen(3000, () => {
     console.log("listening!");
